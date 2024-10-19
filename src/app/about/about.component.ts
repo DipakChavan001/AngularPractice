da0942561d7f0265604f53cb7ed161e006ed1eaa
Dipak Chavan <dipakchavan68@gmail.com>
2024-10-19 15:48:24 +0530

    Submit button and Validation

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 //import {ActivatedRoute } from '@angular/router';
 //import e from 'express';
 //import { validateHeaderName } from 'http';
